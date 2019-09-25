a636ded9cfe83158283c9e65d7a2c1a67fb1b2d9
juancarloss98 <juancarloss@unicauca.edu.co>
2019-09-25 14:14:27 -0500

    version 1

---
+++ src/co/unicauca/tallerpolimorfismo/logica/ViajeIndividual.java
@@ -0,0 +1,15 @@
+package co.unicauca.tallerpolimorfismo.logica;
+import java.util.Date;
+
+public class ViajeIndividual extends Viaje{
+
+    public ViajeIndividual(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+    @Override
+   
+    public String descripcion(){
+        return "Disfruta tu viaje individual";
+    }
+    //No se describe cualquierMetodo2()
+}
---
+++ src/co/unicauca/tallerpolimorfismo/logica/ViajeTodoIncluido.java
@@ -0,0 +1,16 @@
+package co.unicauca.tallerpolimorfismo.logica;
+
+import java.util.Date;
+
+public class ViajeTodoIncluido extends Viaje{
+
+    public ViajeTodoIncluido(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+
+    @Override
+    public String descripcion() {
+        return "Disfruta tu viaje todo incluido";
+    }
+    //No se sobreescribe cualquierMetodo2()
+}
